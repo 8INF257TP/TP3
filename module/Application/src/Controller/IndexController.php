158e445973c079edeca32e49e273c8f488b8c22d
pmlaforest <pmlaforest@hotmail.com>
2018-03-25 23:09:48 -0400

    catalog view

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -19,7 +19,35 @@ class IndexController extends AbstractActionController
 
     public function catalogAction()
     {
-        return new ViewModel();
+        $products = [
+            [
+              'id' => 1,
+              'name' => 'Digital Camera',
+              'price' => 99.95,
+            ],
+            [
+              'id' => 2,
+              'name' => 'Tripod',
+              'price' => 29.95,
+            ],
+            [
+              'id' => 3,
+              'name' => 'Camera Case',
+              'price' => 2.99,
+            ],
+            [
+              'id' => 4,
+              'name' => 'Batteries',
+              'price' => 39.99,
+            ],
+            [
+              'id' => 5,
+              'name' => 'Charger',
+              'price' => 29.99,
+            ],
+          ];
+            
+        return new ViewModel(['products' => $products]);
     }
 
     public function signinAction()
